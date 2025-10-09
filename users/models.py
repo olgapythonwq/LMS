91de17844682c5c0feed8697fad2337b04c2d56b
Olga Python <olga_python@proton.me>
2025-10-09 23:52:20 +0200

    add users files

---
+++ users/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name="Email", help_text="Enter email")
+    phone = models.CharField(max_length=35, blank=True, null=True, verbose_name="Phone", help_text="Enter phone")
+    city = models.CharField(max_length=35, blank=True, null=True, verbose_name="City", help_text="Enter city")
+    avatar = models.ImageField(upload_to="users/avatars", blank=True, null=True, verbose_name="Avatar",
+                               help_text="Upload avatar")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from rest_framework.routers import SimpleRouter
+
+from users.apps import UsersConfig
+from users.views import UserViewSet
+
+
+app_name = UsersConfig.name
+
+router = SimpleRouter()
+router.register('', UserViewSet)
+
+urlpatterns = router.urls
