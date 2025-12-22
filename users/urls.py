8b07328393829a4dd17b486682a3778bf7ecec43
Olga Python <olga_python@proton.me>
2025-12-22 12:42:24 +0100

    maj Users

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 from users.apps import UsersConfig
-from users.views import UserViewSet, PaymentListAPIView
+from users.views import UserViewSet, PaymentListAPIView, UserCreateAPIView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 app_name = UsersConfig.name
 
@@ -11,7 +13,10 @@ router = SimpleRouter()
 router.register('', UserViewSet)
 
 urlpatterns = [
+    path('register/', UserCreateAPIView.as_view(), name='register'),
     path('payments/', PaymentListAPIView.as_view(), name='payments_list'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 urlpatterns += router.urls
--- users/views.py
+++ users/views.py
@@ -1,20 +1,44 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from users.filters import PaymentFilter
 from users.models import User, Payment
-from users.serializers import UserSerializer, PaymentSerializer, UserDetailSerializer
+from users.permissions import IsSelf
+from users.serializers import UserSerializer, PaymentSerializer, UserPrivateSerializer, UserPublicSerializer
+
+
+class UserCreateAPIView(CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)  # позволяет неавторизованным пользователям зарегистрироваться
 
 
 class UserViewSet(ModelViewSet):
     queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
 
     def get_serializer_class(self):
+        # создание пользователя
+        if self.action == "create":
+            return UserSerializer
+        # просмотр профиля
         if self.action == 'retrieve':
-            return UserDetailSerializer
-        return UserSerializer
+            user = self.get_object()
+            if self.request.user == user:
+                return UserPrivateSerializer
+            return UserPublicSerializer
+        # обновление пользователя
+        if self.action in ["update", "partial_update"]:
+            return UserPrivateSerializer
+        return UserPublicSerializer
+
+    def get_permissions(self):
+        if self.action in ["update", "partial_update", "destroy"]:
+            return [IsAuthenticated(), IsSelf()]
+        return [IsAuthenticated()]
 
 
 class PaymentListAPIView(ListAPIView):
