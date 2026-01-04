c248b979cc6cbabf0cf31d2c0e97db7f7c255691
Olga Python <olga_python@proton.me>
2026-01-04 14:56:25 +0100

    updated admin.py files for materials and users

--- materials/admin.py
+++ materials/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from materials.models import Course, Lesson, Subscription
+from users.models import Payment
 
-# Register your models here.
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'owner', 'price')
+    search_fields = ('name',)
+
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'course', 'owner')
+    search_fields = ('name',)
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'course')
+    search_fields = ('user__email', 'course__name')
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'course', 'payment_amount')
+    search_fields = ('user__email', 'course__name')
--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth import get_user_model
 
-from users.models import User
+User = get_user_model()
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_filter = ("id", "email")
+class CustomUserAdmin(UserAdmin):
+    model = User
+
+    list_display = (
+        'id',
+        'email',
+        'is_active',
+        'is_staff',
+        'is_superuser',
+        'last_login',
+    )
+
+    list_filter = (
+        'is_active',
+        'is_staff',
+    )
+
+    ordering = ('email',)
+    search_fields = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'phone', 'city', 'avatar')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
